69dd1ad53543369d1485d6054c1d6a929e993d23
DongHyuk Kim <sss989870@gmail.com>
2017-04-12 15:31:29 +0900

    initial commit

---
+++ GLKit Study/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GLKit Study
+//
+//  Created by 김동혁 on 2017. 4. 12..
+//  Copyright © 2017년 dhyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
